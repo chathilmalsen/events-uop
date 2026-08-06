8181f783aa672099ed0c6791208079fc3f5fd1fe
Chathil Malsen <ktchathilmalsencm@gmail.com>
2026-08-06 17:45:29 +0530

    l

--- src/App.jsx
+++ src/App.jsx
@@ -2719,10 +2719,9 @@ export default function App() {
             <button
               onClick={() => setShowAdd(true)}
               className="flex items-center gap-1.5 px-4 py-2 rounded-full text-sm font-semibold transition-transform active:scale-95 shadow-md hover:shadow-lg"
-              style={{ backgroundColor: "#00e5ff", color: "#05080e" }}
+              style={{ backgroundColor: "#1b1b4a", color: "#707a8c"  }}
             >
-              <CalendarPlus size={16} />
-              <span>POST</span>
+              <span className="text-lg">+</span>
             </button>
           </div>
         </div>
@@ -2760,9 +2759,9 @@ export default function App() {
             <button
               onClick={() => setShowAdd(true)}
               className="flex items-center gap-1 px-3 py-1.5 rounded-full text-xs font-semibold transition-transform active:scale-95 shadow-sm flex-shrink-0"
-              style={{ backgroundColor: "#05051b", color: "#707a8c" }}
+              style={{ backgroundColor: "#1b1b4a", color: "#707a8c" }}
             >
-              <span>POST</span>
+              <span className="text-lg">+</span>
             </button>
           </div>
 
